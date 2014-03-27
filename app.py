772355ef656c13001718cbd017dcde56a1bfaf96
wodim <neikokz@gmail.com>
2014-03-27 16:26:19 +0100

    update

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-from wpsc.views import *
 from wpsc.cache import redis, redis_key
+from wpsc.views import *
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8228)
